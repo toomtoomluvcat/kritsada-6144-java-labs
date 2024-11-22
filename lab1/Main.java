90afa0d30264ee0c63750bbe29b6bdbc2bb9ef73
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-11-22 10:43:16 +0700

    folder with photo

---
+++ lab1/Main.java
@@ -0,0 +1,7 @@
+package lab1;
+class Main{
+    public static void main(String[] args){
+        System.out.println("hello my name is kritsada beapthong my id student is 673040614-4");
+        System.out.println("meow meow meow"); //pull here
+    }
+}

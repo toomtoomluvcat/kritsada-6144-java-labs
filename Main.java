2527be2201ceb440541ac5c4bfab90363622b45e
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-11-22 10:22:13 +0700

    new line

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args){
         System.out.println("hello my name is kritsada beapthong my id student is 673040614-4");
+        System.out.println("meow meow meow"); //pull here
     }
 }

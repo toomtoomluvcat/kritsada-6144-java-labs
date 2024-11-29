518abb4c94079f50c5ba3e528e99fb3003ac8c1d
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-11-29 22:35:30 +0700

    latest update on 11/29/2024 10:35

--- lab2/beapthong/kritsada/lab2/Dealer.java
+++ lab2/beapthong/kritsada/lab2/Dealer.java
@@ -1,21 +1,24 @@
-package beapthong.kritsada.lab2;
-    /*
-    * the Dealer program :
-    * this program accepts three argument then process
-    and display information the out put should be
-    Dealer's name <delaer name>
-    Number of client:<number client>
-    Gender : <dealer_gender>
-    
-    Auther:Kritsada beapthong
-    ID:673040614-4
-    Sec:2*/
+    package beapthong.kritsada.lab2;
+        /*
+        * the Dealer program :
+        * this program accepts three argument then process
+        and display information the out put should be
+        Dealer's name <delaer name>
+        Number of client:<number client>
+        Gender : <dealer_gender>
+        
+        Auther:Kritsada beapthong
+        ID:673040614-4
+        Sec:2*/
 
-public class Dealer { 
-    public static void main(String[] args) {
-        int money = Integer.parseInt(args[1]);
-        System.out.println("Dealer's name: "+ args[0]);
-        System.out.println("Number of client: "+money);
-        System.out.println("Gender: "+args[2]);
-    }
-}
+    public class Dealer { 
+        public static void main(String[] args) {
+            if (args.length != 3) {
+                System.out.println("Error Invalid number of arguments. Please provide exactly three arguments");
+            }else {
+            int money = Integer.parseInt(args[1]);
+            System.out.println("Dealer's name: "+ args[0]);
+            System.out.println("Number of client: "+money);
+            System.out.println("Gender: "+args[2]);}}
+
+        }

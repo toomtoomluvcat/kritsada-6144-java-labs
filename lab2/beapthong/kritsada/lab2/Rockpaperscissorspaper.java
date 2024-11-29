63045d738e805a5e14e294303491363dbbc67525
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-11-29 11:30:31 +0700

    last update 11/29/2024

--- lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
+++ lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
@@ -1,5 +1,6 @@
 package lab2.beapthong.kritsada.lab2;
- /*
+
+/*
   * The Rock paperscisorspaperprogram 
   this program  will accepts argrument of two user 
   than will process follow system
@@ -13,10 +14,36 @@ package lab2.beapthong.kritsada.lab2;
     ID:673040614-4
     Sec:2*/
 public class Rockpaperscissorspaper {
-    public static void main(String[] args){
-    String fristPlayer = args[0];
-    String secoundPlayer = args[1];
-    if ()
+
+    public static void main(String[] args) {
+        boolean checkRockPlayer1 = args[0].equalsIgnoreCase("rock");
+        boolean checkScissorsPlayer1 = args[0].equalsIgnoreCase("scissors");
+        if (args[0].equalsIgnoreCase("rock")) {
+            if (checkRockPlayer1) {
+                System.out.println("2 Player draw!");
+            } else if (checkScissorsPlayer1) {
+                System.out.println("Player 2 win!");
+            } else {
+                System.out.println("Player 1 win!");
+            }
+        }if (args[1].equalsIgnoreCase("paper")){
+            if (checkRockPlayer1){
+                System.out.println("Player 2 win!");
+            } else if (checkScissorsPlayer1){
+                System.out.println("Player 1 win!");
+            } else {
+                System.out.println("2 Player draw");
+            }
+        } else{
+            if (checkRockPlayer1){
+                System.out.println("Player 1 win!");
+            
+            } else if (checkScissorsPlayer1){
+                System.err.println("2 Player draw");
+            } else {
+                System.out.println("Player 2 win!");
+            }
+        }
 
     }
 }

b12465eaee5af8ca874b872a87a662f0c31e2366
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-11-29 10:56:03 +0700

    first commit

---
+++ lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
@@ -0,0 +1,22 @@
+package lab2.beapthong.kritsada.lab2;
+ /*
+  * The Rock paperscisorspaperprogram 
+  this program  will accepts argrument of two user 
+  than will process follow system
+  paper win rock
+  rock win scissors
+  scirssors win papar
+  and display user who win
+  
+  
+  Auther:Kritsada beapthong
+    ID:673040614-4
+    Sec:2*/
+public class Rockpaperscissorspaper {
+    public static void main(String[] args){
+    String fristPlayer = args[0];
+    String secoundPlayer = args[1];
+    if ()
+
+    }
+}

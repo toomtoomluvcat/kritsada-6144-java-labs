3758321a8438e8a249cf70c53029355405dfa1f8
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-11-30 07:57:39 +0700

    add new problem solve

--- lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
+++ lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
@@ -2,48 +2,41 @@ package lab2.beapthong.kritsada.lab2;
 
 /*
   * The Rock paperscisorspaperprogram 
-  this program  will accepts argrument of two user 
+  * this program  will accepts argrument of two user 
   than will process follow system
-  paper win rock
-  rock win scissors
-  scirssors win papar
-  and display user who win
+  * paper win rock
+  * rock win scissors
+  * scirssors win papar
+  * and display user who win 
+  * if program recive input other above data 
+  program would display an error 
   
-  
-  Auther:Kritsada beapthong
-    ID:673040614-4
-    Sec:2*/
+ 
+
 public class Rockpaperscissorspaper {
 
     public static void main(String[] args) {
-        boolean checkRockPlayer1 = args[0].equalsIgnoreCase("rock");
-        boolean checkScissorsPlayer1 = args[0].equalsIgnoreCase("scissors");
-        if (args[0].equalsIgnoreCase("rock")) {
-            if (checkRockPlayer1) {
-                System.out.println("2 Player draw!");
-            } else if (checkScissorsPlayer1) {
-                System.out.println("Player 2 win!");
-            } else {
-                System.out.println("Player 1 win!");
-            }
-        }if (args[1].equalsIgnoreCase("paper")){
-            if (checkRockPlayer1){
-                System.out.println("Player 2 win!");
-            } else if (checkScissorsPlayer1){
-                System.out.println("Player 1 win!");
-            } else {
-                System.out.println("2 Player draw");
-            }
-        } else{
-            if (checkRockPlayer1){
-                System.out.println("Player 1 win!");
-            
-            } else if (checkScissorsPlayer1){
-                System.err.println("2 Player draw");
-            } else {
-                System.out.println("Player 2 win!");
-            }
-        }
+        // declare variable and change to lowercase
+        String Player1 = args[0].toLowerCase();
+        String Player2 = args[1].toLowerCase();
+        //check correct recive input 
+        if ((Player1.equals("rock") || Player1.equals("paper") || Player1.equals("scissors"))
+                && (Player2.equals("rock") || Player2.equals("paper") || Player2.equals("scissors"))) {
 
+            //rule of game
+            if (Player1.equals(Player2)) {
+                System.out.println("it's a tie");
+            } else if ((Player1.equals("scissors") && Player2.equals("paper"))
+                    || Player1.equals("rock") && Player2.equals("paper")
+                    || Player1.equals("paper") && Player2.equals("rock")) {
+                System.out.println("Player 1 win!");
+            } else {
+                System.out.println("Player 2 win!");
+            }
+
+        } else{
+            //display an error
+            System.err.println("Error: Invalid number of argument. Please provide exactly two arguments.");
+        }
     }
 }
---
+++ lab2/beapthong/kritsada/lab2/SimpleStringManipulator.java
@@ -0,0 +1,43 @@
+package lab2.beapthong.kritsada.lab2;
+
+public class SimpleStringManipulator {
+    /* The simple String manipulator 
+     * program recive input two argrument
+     and process 
+     if one first letter of  argrument is vowel 
+     replace it with uppercase vowel
+     if two last letter of arrrument is't vowel
+     replace it with lowercase 
+     
+    Auther:Kritsada beapthong
+    ID:673040614-4
+    Sec:2
+    last update on 11/30/2024 7:57*/
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Invalid number of argument. Please provide exactly two Strings as arguments");
+        } else {
+            String word1 = args[0];
+            String word2 = args[1];
+            char firstLetter = word1.charAt(0);
+            char lastLetter = word2.charAt(args[1].length() - 1);
+
+            System.out.println("First String: " + word1);
+            System.out.println("Secound String: " + word2);
+
+            if (firstLetter == 'a' || firstLetter == 'e'
+                    || firstLetter == 'i' || firstLetter == 'o'
+                    || firstLetter == 'u') {
+                word1 = word1.substring(0, 1).toUpperCase() + word1.substring(1);
+            }
+
+            if (!(lastLetter == 'a' || lastLetter == 'e'
+                    || lastLetter == 'i' || lastLetter == 'o'
+                    || lastLetter == 'u')) {
+                word2 = word2.substring(0, word2.length() - 1) + word2.substring(word2.length() - 1).toUpperCase();
+            }
+
+            System.out.println("Resulting String: "+word1 + word2);
+        }
+    }
+}

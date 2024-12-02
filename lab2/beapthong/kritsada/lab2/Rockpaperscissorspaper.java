b87e06a617971784d2f29d528194d6e45fb87494
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-12-02 16:35:11 +0700

    add check length

--- lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
+++ lab2/beapthong/kritsada/lab2/Rockpaperscissorspaper.java
@@ -20,26 +20,31 @@ public class Rockpaperscissorspaper {
     }
 
     public static void main(String[] args) {
-        // declare variable and change to lowercase
-        String Player1 = args[0].toLowerCase();
-        String Player2 = args[1].toLowerCase();
-        //check correct recive input 
-        try {
-            Choice enumPlayer1 = Choice.valueOf(Player1);
-            Choice enumPlayer2 = Choice.valueOf(Player2);
-            //rule of game
-            if (Player1.equals(Player2)) {
-                System.out.println("it's a tie");
-            } else if ((Player1.equals("scissors") && Player2.equals("paper"))
-                    || Player1.equals("rock") && Player2.equals("scissors")
-                    || Player1.equals("paper") && Player2.equals("rock")) {
-                System.out.println("Player 1 win!");
-            } else {
-                System.out.println("Player 2 win!");
+        if (args.length != 2) {
+            System.out.println("enter more argument");
+        } else {
+            // declare variable and change to lowercase
+            String Player1 = args[0].toLowerCase();
+            String Player2 = args[1].toLowerCase();
+            //check correct recive input 
+            try {
+                Choice enumPlayer1 = Choice.valueOf(Player1);
+                Choice enumPlayer2 = Choice.valueOf(Player2);
+                //rule of game
+                if (Player1.equals(Player2)) {
+                    System.out.println("it's a tie");
+                } else if ((Player1.equals("scissors") && Player2.equals("paper"))
+                        || Player1.equals("rock") && Player2.equals("scissors")
+                        || Player1.equals("paper") && Player2.equals("rock")) {
+                    System.out.println("Player 1 win!");
+                } else {
+                    System.out.println("Player 2 win!");
+                }
+                //display an error
+            } catch (IllegalArgumentException e) {
+                System.err.println("Error: Invalid number of argument. Please provide exactly two arguments.");
             }
-            //display an error
-        } catch (IllegalArgumentException e) {
-            System.err.println("Error: Invalid number of argument. Please provide exactly two arguments.");
         }
+
     }
 }
---
+++ lab3/ConfigurableNumberGuessingGame.java
@@ -0,0 +1,62 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class ConfigurableNumberGuessingGame {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the min value: ");
+        int minvalue = sc.nextInt();
+        int maxvalue;
+
+        while (true) {
+            System.out.print("Enter the max value: ");
+            maxvalue = sc.nextInt();
+            if (maxvalue<=minvalue){
+                System.out.println("The max value must be at least equal to the min value");
+            } else {
+                break;
+            }
+        }
+
+        int numofRound = 0;
+        int attemp; 
+
+        while (true) {
+            System.out.print("Enter of maximum number of tires: "); 
+            attemp =sc.nextInt();
+            if (attemp<1){
+                System.out.println("The maximum number of tries must be greater than 0");
+            }else {
+                break;
+            }    
+        }
+        
+        int randomint = (int) (Math.random()*maxvalue)+minvalue;
+        System.out.println("welcome to a number guessing game!");
+        while(numofRound<attemp){
+            System.out.print("Enter an iteger between "+minvalue+" and "+maxvalue+": ");
+            int chooseint = sc.nextInt();
+            if(chooseint<minvalue || chooseint>maxvalue){
+                System.out.println("The number must be between "+minvalue+" and "+maxvalue);
+                continue;
+            }
+         
+            numofRound +=1;
+
+            if(chooseint>randomint){
+                System.out.println("Try lower number!");
+            } else if (chooseint<randomint){
+                System.out.println("Try higher number!");
+            } else {
+                System.out.println("Congratulations!");
+                System.out.println("Your have tired "+ numofRound+ " time");
+                break;
+            }
+        }
+        if (attemp == numofRound){
+            System.out.println("You have tired 5 times. You ran out of guesses");
+            System.out.println("The answer is "+randomint );
+        }
+    }
+}

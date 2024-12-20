24082a2b85faf351c2caa1944301eee4d4e0b29c
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-12-20 11:00:05 +0700

    abuwwwwwwwwwwwwwwwwwwwwwwwww

---
+++ lab2/beapthong/kritsada/lab5/RecursiveMethods.java
@@ -0,0 +1,79 @@
+package beapthong.kritsada.lab5;
+
+import java.util.Arrays;
+
+public class RecursiveMethods {
+	
+    /**
+     * Recursively finds the sum of digits in a number. Example: For the number
+     * 123, the answer is 1+2+3 = 6
+     *
+     * @param number the input number
+     * @return sum of digits
+     */
+    public static int sumOfDigits(int number) {
+        if (number < 10) {
+            return number;
+        }
+
+        int lastDigit = number % 10;
+        int divideNumber = number / 10;
+        lastDigit += sumOfDigits(divideNumber);
+        return lastDigit;
+    }
+    
+	/**
+ 	* Recursively reverses an array between start and end indices.
+ 	* @param arr the array to reverse
+ 	* @param start starting index
+ 	* @param end ending index
+ 	*/
+
+    public static void reverseArray(int[] arr, int start, int end) {
+        if (start > end) {
+            return;
+        }
+        if (start == end) {
+            System.out.print(arr[start] + " ");
+            return;
+        }
+        System.out.print(arr[end] + " ");
+        reverseArray(arr, start + 1, end - 1);
+        System.out.print(arr[start] + " ");
+    }
+
+    /**
+     * Recursively checks if an array is palindrome.
+     *
+     * @param arr the array to check
+     * @param start starting index
+     * @param end ending index
+     * @return true if palindrome, false otherwise
+     */
+    public static boolean isPalindrome(int[] arr, int start, int end) {
+        Boolean yesIsPalindrome = arr[start] == arr[end];
+		if(!yesIsPalindrome){
+			return yesIsPalindrome;
+		}
+		if (start+1 <= end-1) {
+            yesIsPalindrome = isPalindrome(arr, start + 1, end - 1);
+        }
+        return yesIsPalindrome;
+    }
+
+    public static void main(String[] args) {
+        // Demonstrate each recursive method with example inputs
+        int testingNumber =155;
+        System.out.println();
+        int[] checked = {1,2,3,1};
+        
+        System.out.println(String.format("Sum of digit in %d: %d ",testingNumber, sumOfDigits(testingNumber)));
+
+        System.out.println("Original array:\n"+Arrays.toString(checked));
+        System.out.println("Revese array:");
+        reverseArray(checked, 0, 3);
+
+        System.out.println("\ntesting palindrome: \n"+Arrays.toString(checked));
+		System.out.println(String.format("Is palindrome: %b",isPalindrome(checked, 0, 3)));
+    }
+}
---
+++ lab2/beapthong/kritsada/lab5/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"beapthong.kritsada.lab5"}];updateSearchResults();
